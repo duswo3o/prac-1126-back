6f3919f1d6c964e5deca2dbc05965eced1e3ba14
yeonjae <pyj3740@gmail.com>
2024-11-26 01:50:15 +0900

    feat: signup 패스워드 확인, custom UserManager 정의
    username없이 회원가입을 시도하면서 기존의 create_user를 사용했을때 오류 발생
    따라서 커스텀 유저 매니저를 만들어 사용함

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("이메일은 필수 입력 항목입니다.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -12,6 +34,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    objects = UserManager()  # custom UserManager 사용을 명시
 
     def __str__(self):
         return self.nickname
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,5 @@
+from logging import raiseExceptions
+
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import Serializer, ModelSerializer
 from rest_framework import serializers
@@ -6,7 +8,7 @@ User = get_user_model()
 
 
 class SignUpSerializer(ModelSerializer):
-    confirm_password = serializers.CharField(max_length=20)
+    confirm_password = serializers.CharField(max_length=20, write_only=True)
 
     class Meta:
         model = User
@@ -16,3 +18,18 @@ class SignUpSerializer(ModelSerializer):
             "confirm_password",
             "nickname",
         ]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {"message": "패스워드가 일치하지 않습니다."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            nickname=validated_data["nickname"],
+        )
+        return user
