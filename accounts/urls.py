789a9d8ed6cf544a6405bdcedb70a516e2cd6750
yeonjae <pyj3740@gmail.com>
2024-11-26 10:32:57 +0900

    feat: signout
    refresh 토큰을 blacklist에 추가하여 로그아웃 진행

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView,
 )
 
 from django.urls import path
@@ -10,4 +11,5 @@ urlpatterns = [
     path("", views.signup),
     path("signin/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("signout/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
